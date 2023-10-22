b94574ba3cb70764791d09e442254788dc69d93a
sh <sandrahammamitlili@gmail.com>
2023-10-22 09:58:09 +0200

    auth-google-commit

--- frontend/app/(user)/Appbar.js
+++ frontend/app/(user)/Appbar.js
@@ -1,6 +1,6 @@
 'use client';
 import Link from "next/link";
-import {useSession, signOut,signIn} from 'next-auth/react';
+import {useSession, signOut } from 'next-auth/react';
 
 const AppBar = () => {
     const {data} =useSession();
